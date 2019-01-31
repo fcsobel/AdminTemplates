97b9e7ba17ad7f6f690e5e5247f5711fa7cce2ab
Fred <fcsobel@Yahoo.com>
2019-01-31 16:25:14 -0600

    add new controller

---
+++ src/CoreUI.Mvc/Controllers/ButtonsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminApp.Controllers
+{
+    public class ButtonsController : Controller
+    {
+        public ActionResult Index()
+        {
+			if (Request.IsAjaxRequest())
+			{
+				return PartialView();
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+		public ActionResult Buttons()
+		{
+			if (Request.IsAjaxRequest())
+			{
+				return PartialView();
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+		public ActionResult Dropdowns()
+		{
+			if (Request.IsAjaxRequest())
+			{
+				return PartialView();
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+
+		[ActionName("brand-buttons")]
+		[Route("brand-buttons")]
+		public ActionResult BrandButtons()
+		{
+			if (Request.IsAjaxRequest())
+			{
+				return PartialView();
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+		[ActionName("button-group")]
+		[Route("button-group")]
+
+		public ActionResult ButtonGroup()
+		{
+			if (Request.IsAjaxRequest())
+			{
+				return PartialView();
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+	}
+}
